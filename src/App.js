74aa6dbba2244da30aad38cf2b8428613d5cbb8d
Will Oberley <woberley@DESKTOP-SNQR0JA.localdomain>
2021-09-12 02:32:29 -0400

    react

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
+
+const App = () => {
+  const [showAddTask, setShowAddTask] = useState(false)
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'React',
+        day: 'Feb 5th at 5pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Github',
+        day: 'Feb 8th at 5am',
+        reminder: false,
+    }
+  ])
+  
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+  }
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+  
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task))
+  }
+  return (
+    <div className='container'>
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+      {showAddTask && <AddTask onAdd={addTask} />}
+      {tasks.length > 0 ? ( <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> ) : 'No Tasks To Show'}
+    </div>
+  )
+}
+
+
+export default App;
